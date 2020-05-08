ddd4117a74acff22877d2e1cd009a2bd994e83a3
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-08 01:14:08 -0600

    Refactoring code to ES6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Layout from "./hoc/Layout";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Layout />
     </div>
   );
-}
+};
 
 export default App;
