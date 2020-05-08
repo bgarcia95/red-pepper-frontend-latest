5582502c58cb4a92fef0641fea545b4e32107d8c
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-08 15:08:40 -0600

    Adding method to retrieve supplies on load

--- src/pages/Supplies/Supplies.jsx
+++ src/pages/Supplies/Supplies.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getSuppliesAction } from "../../redux/actions/supplies/supplies";
 
 const Supplies = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getSupplies = () => dispatch(getSuppliesAction());
+    getSupplies();
+  }, [dispatch]);
   return <div>Hey, this is supplies!</div>;
 };
 
