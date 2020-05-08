2f9c236b2649a5d408c956388a98ffb69885cbc8
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-08 14:51:31 -0600

    Connecting redux store with our React app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Layout from "./hoc/Layout";
 import { BrowserRouter } from "react-router-dom";
+import configureStore from "./redux/store/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 const App = () => {
   return (
-    <div className="App">
+    <Provider store={store}>
       <BrowserRouter>
         <Layout />
       </BrowserRouter>
-    </div>
+    </Provider>
   );
 };
 
