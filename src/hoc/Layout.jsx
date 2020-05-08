8f13738c026b7261a5151cc259f3812611326ba5
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-08 01:02:59 -0600

    Adding Layout Component

---
+++ src/hoc/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navigation />
+      <main>Children components</main>
+    </div>
+  );
+};
+
+export default Layout;
