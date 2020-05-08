9af1febbca17eb610d883767a5078cc8008c0243
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-08 01:03:23 -0600

    Adding Layout to out App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Layout from "./hoc/Layout";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <Layout />
     </div>
   );
 }
