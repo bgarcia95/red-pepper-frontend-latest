77dc344859548f661c258d4dc103f78e0ca721a9
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-08 19:16:51 -0600

    Adding components to display Suppliers

--- src/pages/Suppliers/Suppliers.jsx
+++ src/pages/Suppliers/Suppliers.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TableFormat from "../../components/Table/Table";
+import { Typography, Container, Divider } from "@material-ui/core";
+import { getSuppliersAction } from "../../redux/actions/suppliers/suppliers";
 
-const Supppliers = () => {
-  return <div>Hey, this is suppliers!</div>;
+const Suppliers = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getSuppliers = () => dispatch(getSuppliersAction());
+    getSuppliers();
+  }, [dispatch]);
+
+  const suppliers = useSelector((state) => state.suppliers.suppliers);
+  const tableHeaders = ["ID", "Nombre", "Dirección", "Teléfono", "Acciones"];
+
+  return (
+    <React.Fragment>
+      <Container>
+        <Typography variant="h5" align="center">
+          Administración de Proveedores
+        </Typography>
+        <Divider style={{ margin: "2rem 0" }} />
+        <div style={{ margin: "2rem 0" }} />
+        <TableFormat payload={suppliers} tableHeaders={tableHeaders} />
+      </Container>
+    </React.Fragment>
+  );
 };
 
-export default Supppliers;
+export default Suppliers;
