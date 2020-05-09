04ec8454dc38ddf91073fe85cd8c2e77f1a1093b
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-08 18:09:41 -0600

    Adding a modular Table Component and the Supplies page. Right now GET request working properly displaying data from DB.

--- src/pages/Supplies/Supplies.jsx
+++ src/pages/Supplies/Supplies.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getSuppliesAction } from "../../redux/actions/supplies/supplies";
+import SuppliesTable from "../../components/Table/Table";
+import { Typography, Container, Divider } from "@material-ui/core";
 
 const Supplies = () => {
   const dispatch = useDispatch();
@@ -8,7 +10,29 @@ const Supplies = () => {
     const getSupplies = () => dispatch(getSuppliesAction());
     getSupplies();
   }, [dispatch]);
-  return <div>Hey, this is supplies!</div>;
+
+  const supplies = useSelector((state) => state.supplies.supplies);
+  const tableHeaders = [
+    "ID",
+    "Nombre",
+    "Presentación",
+    "Descripción",
+    "Unidad de Medida",
+    "Cantidad Mínima",
+  ];
+
+  return (
+    <React.Fragment>
+      <Container>
+        <Typography variant="h5" align="center">
+          Administración de Insumos
+        </Typography>
+        <Divider style={{ margin: "2rem 0" }} />
+        <div style={{ margin: "2rem 0" }} />
+        <SuppliesTable payload={supplies} tableHeaders={tableHeaders} />
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Supplies;
