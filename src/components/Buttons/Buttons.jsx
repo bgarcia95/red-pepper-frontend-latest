75afd3157a421130c906a25f10320800607e5000
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-09 16:42:54 -0600

    Adding Cancel Button for Dialogs

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,6 +1,18 @@
 import { withStyles, Button } from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
 
 // Action Buttons
+
+export const AddButton = withStyles((theme) => ({
+  root: {
+    color: "white",
+    backgroundColor: green[600],
+    "&:hover": {
+      backgroundColor: green[800],
+    },
+    height: "50px",
+  },
+}))(Button);
 
 export const EditButton = withStyles((theme) => ({
   root: {
@@ -16,5 +28,16 @@ export const DeleteButton = withStyles(() => ({
       backgroundColor: "rgba(255,0,0, 1)",
     },
     height: "30px",
+  },
+}))(Button);
+
+export const CancelButton = withStyles(() => ({
+  root: {
+    color: "white",
+    backgroundColor: "rgba(38, 70, 83, 0.7)",
+    "&:hover": {
+      backgroundColor: "rgba(38, 70, 83, 1)",
+    },
+    height: "50px",
   },
 }))(Button);
