cdedc33b38ceea065d32afc172bd0e7c110b2ef3
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-09 16:46:00 -0600

    Modular Form to be displayed within the Dialog for Supplies

---
+++ src/components/Formik/SuppliesFormik.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import { TextField, FormControl, makeStyles } from "@material-ui/core";
+import { AddButton, CancelButton } from "../Buttons/Buttons";
+import DialogActions from "@material-ui/core/DialogActions";
+
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "auto",
+    width: "fit-content",
+  },
+  formControl: {
+    marginTop: theme.spacing(2),
+    minWidth: 120,
+  },
+  formControlLabel: {
+    marginTop: theme.spacing(1),
+  },
+  center: {
+    margin: "0 auto",
+  },
+}));
+
+const SuppliesFormik = (props) => {
+  const classes = useStyles();
+  const { toggle } = props;
+
+  return (
+    <Formik
+      initialValues={{
+        name: props.supply ? props.supply.name : "",
+        center: classes.center,
+        description: props.supply ? props.supply.description : "",
+        minimumQty: props.supply ? props.supply.minimumQty : "",
+        presentation: props.supply ? props.supply.presentation : "",
+        unitOfMeasure: props.supply ? props.supply.unitOfMeasure : "",
+      }}
+      validationSchema={Yup.object().shape({
+        name: Yup.string().required("Requerido"),
+        presentation: Yup.string().required("Requerido"),
+        description: Yup.string().required("Requerido"),
+        unitOfMeasure: Yup.string().required("Requerido"),
+        minimumQty: Yup.number().required("Requerido"),
+      })}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          isValid,
+        } = props;
+        return (
+          <React.Fragment>
+            <form className={classes.form}>
+              <FormControl fullWidth={true} className={classes.formControl}>
+                <TextField
+                  error={errors.name && touched.name}
+                  id="name"
+                  label="Nombre"
+                  variant="outlined"
+                  value={values.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.name && touched.name
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.name && touched.name && (
+                  <div className="input-feedback">{errors.name}</div>
+                )}
+              </FormControl>
+              <FormControl fullWidth={true} className={classes.formControl}>
+                <TextField
+                  error={errors.presentation && touched.presentation}
+                  id="presentation"
+                  label="Presentación"
+                  variant="outlined"
+                  value={values.presentation}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.presentation && touched.presentation
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.presentation && touched.presentation && (
+                  <div className="input-feedback">{errors.presentation}</div>
+                )}
+              </FormControl>
+              <FormControl fullWidth={true} className={classes.formControl}>
+                <TextField
+                  error={errors.description && touched.description}
+                  id="description"
+                  label="Descripción"
+                  variant="outlined"
+                  value={values.description}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.description && touched.description
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.description && touched.description && (
+                  <div className="input-feedback">{errors.description}</div>
+                )}
+              </FormControl>
+              <FormControl fullWidth={true} className={classes.formControl}>
+                <TextField
+                  error={errors.unitOfMeasure && touched.unitOfMeasure}
+                  id="unitOfMeasure"
+                  label="Unidad de Medida"
+                  variant="outlined"
+                  value={values.unitOfMeasure}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.unitOfMeasure && touched.unitOfMeasure
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.unitOfMeasure && touched.unitOfMeasure && (
+                  <div className="input-feedback">{errors.unitOfMeasure}</div>
+                )}
+              </FormControl>
+              <FormControl fullWidth={true} className={classes.formControl}>
+                <TextField
+                  error={errors.minimumQty && touched.minimumQty}
+                  id="minimumQty"
+                  label="Cantidad Mínima"
+                  variant="outlined"
+                  type="number"
+                  inputProps={{ min: "0", step: "1" }}
+                  value={values.minimumQty}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.minimumQty && touched.minimumQty
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.minimumQty && touched.minimumQty && (
+                  <div className="input-feedback">{errors.minimumQty}</div>
+                )}
+              </FormControl>
+            </form>
+
+            <DialogActions>
+              <div className={classes.center}>
+                <CancelButton onClick={toggle} variant="contained">
+                  Cancelar
+                </CancelButton>
+                <AddButton
+                  onClick={toggle}
+                  variant="contained"
+                  disabled={!dirty || isSubmitting || !isValid}
+                >
+                  Agregar
+                </AddButton>
+              </div>
+            </DialogActions>
+          </React.Fragment>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default SuppliesFormik;
