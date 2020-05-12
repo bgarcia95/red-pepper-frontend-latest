8fc7ee49ba8fc5a5a3a1577bfb6d1ae075e35384
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-11 18:33:33 -0600

    Creating auth file with methods to store and retrieve our token.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,44 @@
 import http from "./httpService";
+import jwtDecode from "jwt-decode";
+
+const apiEndpoint = "/authentication";
+const tokenKey = "token";
+
+export const login = async (user) => {
+  const { data: credentials } = await http.post(`${apiEndpoint}`, user);
+  if (credentials) {
+    localStorage.setItem(tokenKey, credentials.token);
+  }
+  return credentials;
+};
+
+export const trySignUp = () => {
+  let credentials;
+  try {
+    // Obtener el token
+    const token = getJwt();
+    if (token) {
+      const decodedToken = getDecodedToken();
+      credentials = {
+        token,
+        decodedToken,
+      };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return credentials;
+};
+
+const getLocalStorageItem = (key) => localStorage.getItem(key);
+
+export const getJwt = () => getLocalStorageItem(tokenKey);
+
+export const getDecodedToken = () => {
+  try {
+    const token = getJwt();
+    return jwtDecode(token);
+  } catch (error) {
+    return null;
+  }
+};
