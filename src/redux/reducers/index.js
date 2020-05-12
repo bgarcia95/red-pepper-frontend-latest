57030032a519187b80c3d4d2b28da697d0d18bfd
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-11 18:37:50 -0600

    Updating our root reducer.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import suppliesReducer from "./supplies/supplies";
 import suppliersReducer from "./suppliers/suppliers";
+import tokenReducer from "./auth/auth";
 
 export default combineReducers({
   supplies: suppliesReducer,
   suppliers: suppliersReducer,
+  token: tokenReducer,
 });
