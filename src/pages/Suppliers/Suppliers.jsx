8ab453a26d3efb816dcc3cf24fdc84f3c096db02
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-13 14:53:28 -0600

    Refactoring code. Passing props to PageContainer component.

--- src/pages/Suppliers/Suppliers.jsx
+++ src/pages/Suppliers/Suppliers.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import TableFormat from "../../components/Table/TableFormat";
-import { Typography, Container, Divider, Grid } from "@material-ui/core";
 import {
   getSuppliersAction,
   deleteSupplierAction,
   deleteSupplierStart,
 } from "../../redux/actions/suppliers/suppliers";
-import FormDialog from "../../components/Modals/FormDialog";
 import Swal from "sweetalert2";
+import PageContainer from "../../components/PageContainer/PageContainer";
 
 const Suppliers = () => {
   const dispatch = useDispatch();
@@ -52,37 +50,19 @@ const Suppliers = () => {
   const isLoadingData = useSelector((state) => state.suppliers.error);
 
   return (
-    <React.Fragment>
-      <Container>
-        <Typography variant="h5" align="center">
-          Administración de Proveedores
-        </Typography>
-        <Divider style={{ margin: "2rem 0" }} />
-
-        <Grid item xs={12} className="text-center">
-          <FormDialog
-            formTarget={formTarget}
-            buttonLabel="Agregar Proveedor"
-            title="Agregar Proveedor"
-          />
-        </Grid>
-        <div style={{ margin: "2rem 0" }} />
-        {isLoadingData && (
-          <div className="error--message">
-            <p>Hubo un problema cargando la informacion...</p>
-          </div>
-        )}
-        <TableFormat
-          payload={suppliers}
-          tableHeaders={tableHeaders}
-          formTarget={formTarget}
-          onDelete={onDelete}
-          isLoading={isLoading}
-          isProcessing={isProcessing}
-          isFetching={isFetching}
-        />
-      </Container>
-    </React.Fragment>
+    <PageContainer
+      pageTitle="Administración de Proveedores"
+      suppliers={suppliers}
+      formTarget={formTarget}
+      tableHeaders={tableHeaders}
+      isLoading={isLoading}
+      isProcessing={isProcessing}
+      isFetching={isFetching}
+      isLoadingData={isLoadingData}
+      buttonLabel="Agregar Proveedor"
+      dialogTitle="Agregar Proveedor"
+      onDelete={onDelete}
+    />
   );
 };
 
