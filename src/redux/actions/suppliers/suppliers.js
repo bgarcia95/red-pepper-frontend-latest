80e622c2b8153adc1fcecc23cdad2daa7e93e32a
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-13 01:12:54 -0600

    Adding reducer and action methods for suppliers.

--- src/redux/actions/suppliers/suppliers.js
+++ src/redux/actions/suppliers/suppliers.js
@@ -3,6 +3,15 @@ import {
   GET_SUPPLIERS_START,
   GET_SUPPLIERS_SUCCESS,
   GET_SUPPLIERS_ERROR,
+  ADD_SUPPLIER_START,
+  ADD_SUPPLIER_ERROR,
+  ADD_SUPPLIER_SUCCESS,
+  UPDATE_SUPPLIER_START,
+  UPDATE_SUPPLIER_SUCCESS,
+  UPDATE_SUPPLIER_ERROR,
+  DELETE_SUPPLIER_START,
+  DELETE_SUPPLIER_SUCCESS,
+  DELETE_SUPPLIER_ERROR,
 } from "../../utils/actions";
 
 // All GET methods
@@ -33,6 +42,111 @@ export const getSuppliersAction = () => {
       .catch((error) => {
         console.log(error);
         dispatch(getSuppliersError());
+      });
+  };
+};
+
+// ALL POST METHODS
+
+export const addSupplierStart = () => ({
+  type: ADD_SUPPLIER_START,
+});
+
+export const addSupplierSuccess = (supplier) => ({
+  type: ADD_SUPPLIER_SUCCESS,
+  supplier,
+});
+
+export const addSupplierError = (error) => ({
+  type: ADD_SUPPLIER_ERROR,
+  error,
+});
+
+export const addSupplierAction = (supplier) => {
+  return (dispatch) => {
+    dispatch(addSupplierStart());
+
+    // Insert into db
+    http
+      .post("/provider", supplier)
+      .then((response) => {
+        dispatch(addSupplierSuccess(response.data));
+        Swal.fire(
+          "¡Guardado!",
+          "El proveedor fue guardado satisfactoriamente",
+          "success"
+        );
+        dispatch(getSuppliersAction());
+      })
+      .catch((error) => {
+        dispatch(addSupplierError(error));
+      });
+  };
+};
+
+// ALL PUT (PATCH) METHODS
+
+export const updateSupplierStart = () => ({
+  type: UPDATE_SUPPLIER_START,
+});
+
+export const updateSupplierSuccess = (supplier) => ({
+  type: UPDATE_SUPPLIER_SUCCESS,
+  supplier,
+});
+
+export const updateSupplierError = (error) => ({
+  type: UPDATE_SUPPLIER_ERROR,
+  error,
+});
+
+export const updateSupplierAction = (supplier) => {
+  return (dispatch) => {
+    dispatch(updateSupplierStart());
+
+    http
+      .put("/provider", supplier)
+      .then((response) => {
+        dispatch(updateSupplierSuccess(response.data));
+        Swal.fire(
+          "¡Guardado!",
+          "El proveedor fue actualizado satisfactoriamente",
+          "success"
+        );
+        dispatch(getSuppliersAction());
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(updateSupplierError(error));
+      });
+  };
+};
+
+// ALL DELETE METHODS
+
+export const deleteSupplierStart = () => ({
+  type: DELETE_SUPPLIER_START,
+});
+
+export const deleteSupplierSuccess = (id) => ({
+  type: DELETE_SUPPLIER_SUCCESS,
+  id,
+});
+
+export const deleteSupplierError = (error) => ({
+  type: DELETE_SUPPLIER_ERROR,
+  error,
+});
+export const deleteSupplierAction = (id) => {
+  return (dispatch) => {
+    http
+      .delete(`/provider?id=${id}`)
+      .then((response) => {
+        dispatch(deleteSupplierSuccess(response.data.id));
+        dispatch(getSuppliersAction());
+      })
+      .catch((error) => {
+        dispatch(deleteSupplierError(error));
       });
   };
 };
--- src/redux/reducers/suppliers/suppliers.js
+++ src/redux/reducers/suppliers/suppliers.js
@@ -2,10 +2,19 @@ import {
   GET_SUPPLIERS_START,
   GET_SUPPLIERS_SUCCESS,
   GET_SUPPLIERS_ERROR,
+  ADD_SUPPLIER_START,
+  ADD_SUPPLIER_SUCCESS,
+  ADD_SUPPLIER_ERROR,
+  UPDATE_SUPPLIER_START,
+  UPDATE_SUPPLIER_SUCCESS,
+  UPDATE_SUPPLIER_ERROR,
+  DELETE_SUPPLIER_START,
+  DELETE_SUPPLIER_SUCCESS,
+  DELETE_SUPPLIER_ERROR,
 } from "../../utils/actions";
 
 // Default state
-const suppliesDefaultState = {
+const suppliersDefaultState = {
   suppliers: [],
   isLoading: true,
   error: null,
@@ -13,7 +22,7 @@ const suppliesDefaultState = {
   isFetching: false,
 };
 
-export default (state = suppliesDefaultState, action) => {
+export default (state = suppliersDefaultState, action) => {
   switch (action.type) {
     case GET_SUPPLIERS_START:
       return {
@@ -32,6 +41,69 @@ export default (state = suppliesDefaultState, action) => {
       return {
         ...state,
         isLoading: false,
+        isFetching: false,
+        error: true,
+      };
+    case ADD_SUPPLIER_START:
+      return {
+        ...state,
+        error: null,
+        isProcessing: true,
+      };
+    case ADD_SUPPLIER_SUCCESS:
+      return {
+        ...state,
+        suppliers: [...state.suppliers, action.supplier],
+        error: null,
+        isProcessing: false,
+      };
+
+    case ADD_SUPPLIER_ERROR:
+      return {
+        ...state,
+        error: true,
+        isProcessing: false,
+      };
+
+    case UPDATE_SUPPLIER_START:
+      return {
+        ...state,
+        isProcessing: true,
+        error: null,
+        isFetching: false,
+      };
+    case UPDATE_SUPPLIER_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        suppliers: state.supplies.map(
+          (supplier) =>
+            supplier.id === action.supplier.id && (supplier = action.supplier)
+        ),
+        isProcessing: false,
+        isFetching: false,
+      };
+    case UPDATE_SUPPLIER_ERROR:
+      return {
+        ...state,
+        error: true,
+        isProcessing: false,
+        isFetching: false,
+      };
+    case DELETE_SUPPLIER_START:
+      return {
+        ...state,
+        error: null,
+      };
+    case DELETE_SUPPLIER_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        suppliers: state.suppliers.filter(({ id }) => id !== action.id),
+      };
+    case DELETE_SUPPLIER_ERROR:
+      return {
+        ...state,
         error: true,
       };
 
