197fc11c3a54504cf8768e8f3e75a9f2e57d3320
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-13 03:54:59 -0600

    Removing inline css. Adding variables to handle loading animations. Adding onDelete method and passing it as props to our Table component.

--- src/pages/Suppliers/Suppliers.jsx
+++ src/pages/Suppliers/Suppliers.jsx
@@ -1,24 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TableFormat from "../../components/Table/TableFormat";
+import { Typography, Container, Divider, Grid } from "@material-ui/core";
 import {
-  Typography,
-  Container,
-  Divider,
-  Grid,
-  makeStyles,
-} from "@material-ui/core";
-import { getSuppliersAction } from "../../redux/actions/suppliers/suppliers";
+  getSuppliersAction,
+  deleteSupplierAction,
+  deleteSupplierStart,
+} from "../../redux/actions/suppliers/suppliers";
 import FormDialog from "../../components/Modals/FormDialog";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  container: {
-    textAlign: "center",
-  },
-}));
+import Swal from "sweetalert2";
 
 const Suppliers = () => {
   const dispatch = useDispatch();
@@ -27,11 +17,39 @@ const Suppliers = () => {
     getSuppliers();
   }, [dispatch]);
 
-  const classes = useStyles();
   const suppliers = useSelector((state) => state.suppliers.suppliers);
   const tableHeaders = ["ID", "Nombre", "Dirección", "Teléfono", "Acciones"];
 
   const formTarget = "supplier";
+
+  // Remove Supplier
+  const onDelete = (id) => {
+    dispatch(deleteSupplierStart());
+    Swal.fire({
+      title: "¿Estás seguro/a?",
+      text: "Un proveedor eliminado no puede ser restaurado.  ",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "¡Sí, remover!",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.value) {
+        dispatch(deleteSupplierAction(id));
+        Swal.fire(
+          "¡Removido!",
+          "El registro fué removido satisfactoriamente.",
+          "success"
+        );
+      }
+    });
+  };
+
+  // Loading
+  const isLoading = useSelector((state) => state.suppliers.isLoading);
+  const isProcessing = useSelector((state) => state.suppliers.isProcessing);
+  const isFetching = useSelector((state) => state.suppliers.isFetching);
+  const isLoadingData = useSelector((state) => state.suppliers.error);
 
   return (
     <React.Fragment>
@@ -40,7 +58,8 @@ const Suppliers = () => {
           Administración de Proveedores
         </Typography>
         <Divider style={{ margin: "2rem 0" }} />
-        <Grid item xs={12} className={classes.container}>
+
+        <Grid item xs={12} className="text-center">
           <FormDialog
             formTarget={formTarget}
             buttonLabel="Agregar Proveedor"
@@ -48,10 +67,19 @@ const Suppliers = () => {
           />
         </Grid>
         <div style={{ margin: "2rem 0" }} />
+        {isLoadingData && (
+          <div className="error--message">
+            <p>Hubo un problema cargando la informacion...</p>
+          </div>
+        )}
         <TableFormat
           payload={suppliers}
           tableHeaders={tableHeaders}
           formTarget={formTarget}
+          onDelete={onDelete}
+          isLoading={isLoading}
+          isProcessing={isProcessing}
+          isFetching={isFetching}
         />
       </Container>
     </React.Fragment>
