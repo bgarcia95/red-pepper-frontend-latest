a3e7672b13a8e52e92c5406341fe07d7ee38b408
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-13 00:13:03 -0600

    Adding setJwt method to add headers dinamically to our axios requests.

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -5,4 +5,14 @@ const httpService = axios.create({
   baseURL: REACT_APP_SERVER_URL,
 });
 
-export default httpService;
+const setJwt = (jwt) => {
+  httpService.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+};
+
+export default {
+  get: httpService.get,
+  post: httpService.post,
+  put: httpService.put,
+  delete: httpService.delete,
+  setJwt,
+};
