0c7954a9c4b0cf614deb171e6aa753c3d8fef812
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-13 22:40:15 -0600

    Removing table pagination actions code and importing it from own file.

--- src/components/Table/TableFormat.jsx
+++ src/components/Table/TableFormat.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -7,87 +7,13 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableFooter from "@material-ui/core/TableFooter";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
+import TablePaginationActions from "./TablePaginationActions";
 import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
-import FirstPageIcon from "@material-ui/icons/FirstPage";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import LastPageIcon from "@material-ui/icons/LastPage";
 import { TableHead, Backdrop, CircularProgress } from "@material-ui/core";
 import { FaTrash } from "react-icons/fa";
 import { DeleteButton } from "../UI/Buttons/Buttons";
 import FormDialog from "../Modals/FormDialog";
 import { v4 as uuid } from "uuid";
-
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-  },
-}));
-
-const TablePaginationActions = (props) => {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onChangePage } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onChangePage(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onChangePage(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onChangePage(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-};
 
 const useStyles2 = makeStyles((theme) => ({
   table: {
@@ -116,11 +42,12 @@ const TableFormat = (props) => {
     payload,
     tableHeaders,
     formTarget,
-    onDelete,
     isLoading,
     isProcessing,
     isFetching,
+    onDelete,
   } = props;
+
   const emptyRows =
     rowsPerPage - Math.min(rowsPerPage, payload.length - page * rowsPerPage);
 
@@ -141,9 +68,9 @@ const TableFormat = (props) => {
   };
 
   const headerOptions = () => {
-    return tableHeaders.map((field, index) => (
+    return tableHeaders.map((header, index) => (
       <TableCell key={index} align="center">
-        {field}
+        {header.text}
       </TableCell>
     ));
   };
@@ -158,14 +85,17 @@ const TableFormat = (props) => {
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="custom pagination table">
           <TableHead>
-            <TableRow>{headerOptions()}</TableRow>
+            <TableRow>
+              {headerOptions()}
+              <TableCell align="center">Acciones</TableCell>
+            </TableRow>
           </TableHead>
           <TableBody>
             {isLoading || isFetching ? (
               <TableRow key={uuid()}>
                 <TableCell
                   key={uuid()}
-                  colSpan={tableHeaders.length}
+                  colSpan={tableHeaders.length + 1}
                   rowSpan={rowsPerPage}
                   style={{ textAlign: "center" }}
                 >
@@ -192,19 +122,15 @@ const TableFormat = (props) => {
                       page * rowsPerPage + rowsPerPage
                     )
                   : payload
-                ).map((item) => {
-                  let keyValues = [];
-                  for (const key in item) {
-                    keyValues.push(key);
-                  }
-                  return (
-                    <TableRow key={uuid()}>
-                      {keyValues.map((key) => (
-                        <TableCell key={uuid()} align="center">
-                          {item[key]}
-                        </TableCell>
-                      ))}
-                      <TableCell>
+                ).map((item) => (
+                  <TableRow key={item.id}>
+                    {tableHeaders.map(({ field }) => (
+                      <TableCell key={uuid()} align="center">
+                        {item[field]}
+                      </TableCell>
+                    ))}
+                    <TableCell key={uuid()}>
+                      {onDelete ? (
                         <div className={classes.buttonContainer}>
                           <FormDialog
                             formTarget={formTarget}
@@ -227,10 +153,18 @@ const TableFormat = (props) => {
                             <FaTrash />
                           </DeleteButton>
                         </div>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
+                      ) : (
+                        <div className="text-center">
+                          <FormDialog
+                            formTarget={formTarget}
+                            buttonLabel="Ver"
+                            payload={item}
+                          />
+                        </div>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
               </React.Fragment>
             )}
 
@@ -248,7 +182,7 @@ const TableFormat = (props) => {
             <TableRow>
               <TablePagination
                 rowsPerPageOptions={[5, 10, 25, { label: "Todos", value: -1 }]}
-                colSpan={tableHeaders.length}
+                colSpan={tableHeaders.length + 1}
                 count={payload.length}
                 rowsPerPage={rowsPerPage}
                 page={page > 0 && payload.length === rowsPerPage ? 0 : page}
