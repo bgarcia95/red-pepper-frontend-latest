c6bcf860c4deb66670a8a76fb3ef68256d41ea0e
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-13 22:45:36 -0600

    Adding Purchase Form. Adding Watch Button forto show on Purchases Table. Updated way to output dialog title.

--- src/components/Modals/FormDialog.jsx
+++ src/components/Modals/FormDialog.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import { EditButton, AddButton, WatchButton } from "../UI/Buttons/Buttons";
+import { FaEdit, FaEye } from "react-icons/fa";
 import SuppliesFormik from "../Formik/SuppliesFormik";
-import { EditButton, AddButton } from "../UI/Buttons/Buttons";
-import { FaEdit } from "react-icons/fa";
 import SuppliersFormik from "../Formik/SuppliersFormik";
+import PurchasesFormik from "../Formik/PurchasesFormik";
 
 const FormDialog = (props) => {
   const { buttonLabel } = props;
@@ -26,6 +27,12 @@ const FormDialog = (props) => {
         <FaEdit size="18" />
       </EditButton>
     );
+  } else if (buttonLabel === "Ver") {
+    button = (
+      <WatchButton onClick={toggleModal} color="primary" variant="contained">
+        <FaEye size="18" />
+      </WatchButton>
+    );
   } else {
     button = (
       <AddButton variant="contained" onClick={toggleModal}>
@@ -35,19 +42,24 @@ const FormDialog = (props) => {
   }
 
   let form;
-  let sufix = "";
+  let customTitle = "";
   let dialogSize = "";
 
   switch (props.formTarget) {
     case "supply":
       form = <SuppliesFormik {...props} toggle={toggleModal} modal={open} />;
-      sufix = "Insumo";
+      customTitle = "Editar Insumo";
       dialogSize = "md";
       break;
     case "supplier":
       form = <SuppliersFormik {...props} toggle={toggleModal} modal={open} />;
-      sufix = "Proveedor";
+      customTitle = "Editar Proveedor";
       dialogSize = "sm";
+      break;
+    case "purchase":
+      form = <PurchasesFormik {...props} toggle={toggleModal} modal={open} />;
+      customTitle = "Ver Factura";
+      dialogSize = "xl";
       break;
     default:
       break;
@@ -64,7 +76,7 @@ const FormDialog = (props) => {
         fullWidth={true}
       >
         <DialogTitle id="form-dialog-title" className="text-center">
-          {title ? title : `Editar ${sufix}`}
+          {title ? title : customTitle}
         </DialogTitle>
         <DialogContent>{form}</DialogContent>
       </Dialog>
