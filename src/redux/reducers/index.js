a4bfe901e3cb84aef6f1729aca2564869b3094e2
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-13 22:35:21 -0600

    Adding purchases reducer to index reducer.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import suppliesReducer from "./supplies/supplies";
 import suppliersReducer from "./suppliers/suppliers";
+import purchasesReducer from "./supplies-purchases/purchases";
 import tokenReducer from "./auth/auth";
 
 export default combineReducers({
   supplies: suppliesReducer,
   suppliers: suppliersReducer,
+  purchases: purchasesReducer,
   auth: tokenReducer,
 });
