482fbc051edd68ce5b12020a6183e140ef43c664
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-17 01:12:26 -0600

    Added Category page content

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getCategoriesAction,
+  deleteCategoryAction,
+  deleteCategoryStart,
+} from "../../redux/actions/categories/categories";
+import Swal from "sweetalert2";
+import PageContainer from "../../components/PageContainer/PageContainer";
 
 const Categories = () => {
-  return <div>Hey, this is categories!</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getCategories = () => dispatch(getCategoriesAction());
+    getCategories();
+  }, [dispatch]);
+
+  const categories = useSelector((state) => state.categories.categories);
+  const tableHeaders = [
+    { text: "ID", field: "id" },
+    { text: "Categoría", field: "name" },
+    { text: "Descripción", field: "description" },
+  ];
+
+  const formTarget = "category";
+
+  // Remove Supplier
+  const onDelete = (id) => {
+    dispatch(deleteCategoryStart());
+    Swal.fire({
+      title: "¿Estás seguro/a?",
+      text: "Una categoría eliminada no puede ser restaurada.  ",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "¡Sí, remover!",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.value) {
+        dispatch(deleteCategoryAction(id));
+        Swal.fire(
+          "¡Removido!",
+          "El registro fué removido satisfactoriamente.",
+          "success"
+        );
+      }
+    });
+  };
+
+  // Loading
+  const isLoading = useSelector((state) => state.categories.isLoading);
+  const isProcessing = useSelector((state) => state.categories.isProcessing);
+  const isFetching = useSelector((state) => state.categories.isFetching);
+  const isLoadingData = useSelector((state) => state.categories.error);
+
+  return (
+    <PageContainer
+      pageTitle="Administración de Categorías"
+      payload={categories}
+      formTarget={formTarget}
+      tableHeaders={tableHeaders}
+      isLoading={isLoading}
+      isProcessing={isProcessing}
+      isFetching={isFetching}
+      isLoadingData={isLoadingData}
+      buttonLabel="Agregar Categoría"
+      dialogTitle="Agregar Categoría"
+      onDelete={onDelete}
+    />
+  );
 };
 
 export default Categories;
