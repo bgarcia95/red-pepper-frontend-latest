9c4904d06c085ff19a437cb348feb96073d16742
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-17 01:08:35 -0600

    Adding categories reducer to redux store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import suppliesReducer from "./supplies/supplies";
 import suppliersReducer from "./suppliers/suppliers";
 import purchasesReducer from "./supplies-purchases/purchases";
+import categoriesReducer from "./categories/categories";
 import tokenReducer from "./auth/auth";
 
 export default combineReducers({
   supplies: suppliesReducer,
   suppliers: suppliersReducer,
   purchases: purchasesReducer,
+  categories: categoriesReducer,
   auth: tokenReducer,
 });
