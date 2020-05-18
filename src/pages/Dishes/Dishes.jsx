dc0d71fb781187e6e369fcf5184893e8a8d03867
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-18 00:45:36 -0600

    Adding Dishes page

--- src/pages/Dishes/Dishes.jsx
+++ src/pages/Dishes/Dishes.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getDishesAction,
+  deleteDishStart,
+  deleteDishAction,
+} from "../../redux/actions/dishes/dishes";
+import Swal from "sweetalert2";
+import PageContainer from "../../components/PageContainer/PageContainer";
+import { getCategoriesAction } from "../../redux/actions/categories/categories";
 
 const Dishes = () => {
-  return <div>Hey, this is dishes!</div>;
+  const dispatch = useDispatch();
+
+  const dishes = useSelector((state) => state.dishes.dishes);
+  const categories = useSelector((state) => state.categories.categories);
+
+  useEffect(() => {
+    const getDishes = () => dispatch(getDishesAction());
+    getDishes();
+    const getCategories = () => dispatch(getCategoriesAction());
+    getCategories();
+  }, [dispatch]);
+
+  const tableHeaders = [
+    { text: "ID", field: "id" },
+    { text: "Nombre", field: "name" },
+    { text: "Descripción", field: "description" },
+    { text: "Precio", field: "price" },
+  ];
+
+  const formTarget = "dish";
+
+  // Remove Supply
+  const onDelete = (id) => {
+    dispatch(deleteDishStart());
+    Swal.fire({
+      title: "¿Estás seguro/a?",
+      text: "Un platillo eliminado no puede ser restaurado.  ",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "¡Sí, remover!",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.value) {
+        dispatch(deleteDishAction(id));
+        Swal.fire(
+          "¡Removido!",
+          "El registro fué removido satisfactoriamente.",
+          "success"
+        );
+      }
+    });
+  };
+
+  // Loading
+  const isLoading = useSelector((state) => state.dishes.isLoading);
+  const isProcessing = useSelector((state) => state.dishes.isProcessing);
+  const isFetching = useSelector((state) => state.dishes.isFetching);
+  const isLoadingData = useSelector((state) => state.dishes.error);
+
+  return (
+    <PageContainer
+      pageTitle="Administración de Platillos"
+      payload={dishes}
+      categories={categories}
+      formTarget={formTarget}
+      tableHeaders={tableHeaders}
+      isLoading={isLoading}
+      isProcessing={isProcessing}
+      isFetching={isFetching}
+      isLoadingData={isLoadingData}
+      buttonLabel="Agregar Platillo"
+      dialogTitle="Agregar Platillo"
+      onDelete={onDelete}
+    />
+  );
 };
 
 export default Dishes;
