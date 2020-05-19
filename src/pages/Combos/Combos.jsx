4000177c504116f7bdb10ee4e44507eec1bfa9cf
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-18 21:00:47 -0600

    Adding Combos page.

--- src/pages/Combos/Combos.jsx
+++ src/pages/Combos/Combos.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import PageContainer from "../../components/PageContainer/PageContainer";
+import {
+  getCombosAction,
+  deleteComboStart,
+  deleteComboAction,
+} from "../../redux/actions/combos/combos";
 
 const Combos = () => {
-  return <div>Hey, this is combos!</div>;
+  const dispatch = useDispatch();
+
+  const combos = useSelector((state) => state.combos.combos);
+
+  useEffect(() => {
+    const getCombos = () => dispatch(getCombosAction());
+    getCombos();
+  }, [dispatch]);
+
+  const tableHeaders = [
+    { text: "ID", field: "id" },
+    { text: "Nombre", field: "name" },
+    { text: "Descripción", field: "description" },
+    { text: "Total", field: "total" },
+  ];
+
+  const formTarget = "combo";
+
+  // Remove Combo
+  const onDelete = (id) => {
+    dispatch(deleteComboStart());
+    Swal.fire({
+      title: "¿Estás seguro/a?",
+      text: "Un combo eliminado no puede ser restaurado.",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "¡Sí, remover!",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.value) {
+        dispatch(deleteComboAction(id));
+        Swal.fire(
+          "¡Removido!",
+          "El registro fué removido satisfactoriamente.",
+          "success"
+        );
+      }
+    });
+  };
+
+  // Loading
+  const isLoading = useSelector((state) => state.combos.isLoading);
+  const isProcessing = useSelector((state) => state.combos.isProcessing);
+  const isFetching = useSelector((state) => state.combos.isFetching);
+  const isLoadingData = useSelector((state) => state.combos.error);
+
+  return (
+    <PageContainer
+      pageTitle="Administración de Combos"
+      payload={combos}
+      formTarget={formTarget}
+      tableHeaders={tableHeaders}
+      isLoading={isLoading}
+      isProcessing={isProcessing}
+      isFetching={isFetching}
+      isLoadingData={isLoadingData}
+      buttonLabel="Agregar Combo"
+      dialogTitle="Agregar Combo"
+      onDelete={onDelete}
+    />
+  );
 };
 
 export default Combos;
