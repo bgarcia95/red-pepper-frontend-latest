b22e199c52b9e46a21be1233650ee363db0f365e
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-18 23:06:37 -0600

    Adding table page

--- src/pages/Tables/Tables.jsx
+++ src/pages/Tables/Tables.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Swal from "sweetalert2";
+import PageContainer from "../../components/PageContainer/PageContainer";
+import {
+  deleteTableStart,
+  deleteTableAction,
+  getTablesAction,
+} from "../../redux/actions/tables/tables";
 
 const Tables = () => {
-  return <div>Hey, this is tables!</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getTables = () => dispatch(getTablesAction());
+    getTables();
+  }, [dispatch]);
+
+  const tables = useSelector((state) => state.tables.tables);
+
+  const tableHeaders = [
+    { text: "ID", field: "id" },
+    { text: "Nombre", field: "name" },
+    { text: "Descripción", field: "description" },
+    { text: "N° de Sillas", field: "chairs" },
+  ];
+
+  const formTarget = "table";
+
+  // Remove Table
+  const onDelete = (id) => {
+    dispatch(deleteTableStart());
+    Swal.fire({
+      title: "¿Estás seguro/a?",
+      text: "Una mesa eliminada no puede ser restaurada.",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "¡Sí, remover!",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.value) {
+        dispatch(deleteTableAction(id));
+        Swal.fire(
+          "¡Removido!",
+          "El registro fué removido satisfactoriamente.",
+          "success"
+        );
+      }
+    });
+  };
+
+  // Loading
+  const isLoading = useSelector((state) => state.tables.isLoading);
+  const isProcessing = useSelector((state) => state.tables.isProcessing);
+  const isFetching = useSelector((state) => state.tables.isFetching);
+  const isLoadingData = useSelector((state) => state.tables.error);
+
+  return (
+    <PageContainer
+      pageTitle="Administración de Mesas"
+      payload={tables}
+      formTarget={formTarget}
+      tableHeaders={tableHeaders}
+      isLoading={isLoading}
+      isProcessing={isProcessing}
+      isFetching={isFetching}
+      isLoadingData={isLoadingData}
+      buttonLabel="Agregar Mesa"
+      dialogTitle="Agregar Mesa"
+      onDelete={onDelete}
+    />
+  );
 };
 
 export default Tables;
