1f2efc4e388e922a340630b52acc94e75b2870ed
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-20 03:56:21 -0600

    Adding backdrop with spinner and message when logging in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Layout from "./hoc/Layout";
 import { trySignUp } from "./redux/actions/auth/auth";
+import {
+  makeStyles,
+  Backdrop,
+  CircularProgress,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles2 = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "blue",
+  },
+}));
 
 const App = () => {
   const dispatch = useDispatch();
+  const classes = useStyles2();
+
+  const isLogging = useSelector((state) => state.auth.isLogging);
 
   useEffect(() => {
     const onTrySignUp = () => dispatch(trySignUp());
@@ -13,6 +29,23 @@ const App = () => {
 
   return (
     <React.Fragment>
+      {isLogging ? (
+        <Backdrop id="myBackdrop" className={classes.backdrop} open={true}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CircularProgress style={{ color: "white" }} />
+            <Typography style={{ color: "#fff" }} variant="h5">
+              Iniciando Sesión..
+            </Typography>
+          </div>
+        </Backdrop>
+      ) : null}
       <Layout />
     </React.Fragment>
   );
