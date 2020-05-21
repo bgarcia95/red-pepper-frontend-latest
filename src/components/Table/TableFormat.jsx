bd47dce61c0eb704798ad9dbb1c399d4def0c1cb
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-21 03:49:08 -0600

    Implementing server side pagination and removing unused imports. Also moved Add button into this file.

--- src/components/Table/TableFormat.jsx
+++ src/components/Table/TableFormat.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { Backdrop, CircularProgress } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import MaterialTable from "material-table";
 import FormDialog from "components/Modals/FormDialog";
 import { DeleteButton } from "components/UI/Buttons/Buttons";
 import { FaTrash } from "react-icons/fa";
+import httpService from "services/httpService";
+import { useRef } from "react";
 
 const useStyles2 = makeStyles((theme) => ({
   backdrop: {
@@ -16,31 +18,56 @@ const useStyles2 = makeStyles((theme) => ({
 
 const TableFormat = (props) => {
   const classes = useStyles2();
-  const {
-    payload,
-    tableHeaders,
-    formTarget,
-    isLoading,
-    isProcessing,
-    isFetching,
-    onDelete,
-    categories,
-    tableTitle,
-  } = props;
+  const tableRef = useRef();
+  const refresh = () => {
+    console.log(
+      "Did refresh!",
+      tableRef.current && tableRef.current.onQueryChange()
+    );
+
+    return tableRef.current && tableRef.current.onQueryChange();
+  };
+
+  const { tableHeaders, formTarget, onDelete, categories, tableTitle } = props;
 
   return (
     <React.Fragment>
-      {isProcessing && (
+      {/*{isProcessing && (
         <Backdrop id="myBackdrop" className={classes.backdrop} open={true}>
           <CircularProgress color="inherit" />
         </Backdrop>
-      )}
+      )}*/}
+      <Grid item xs={12} className="text-center">
+        <FormDialog
+          formTarget={props.formTarget}
+          buttonLabel={props.buttonLabel}
+          title={props.dialogTitle}
+          categories={props.formTarget === "dish" && props.categories}
+          onRefresh={refresh}
+        />
+      </Grid>
+      <div style={{ margin: "2rem 0" }} />
       <MaterialTable
         className={classes.table}
         columns={tableHeaders}
-        data={payload}
+        tableRef={tableRef}
+        data={(query) =>
+          new Promise(async (resolve, reject) => {
+            const response = await httpService.get(
+              `/provider?page=${query.page + 1}&size=${query.pageSize}`
+            );
+            const headersToJson = JSON.parse(response.headers["x-pagination"]);
+
+            resolve({
+              data: response.data,
+              page: headersToJson.Page - 1,
+              totalCount: headersToJson.TotalCount,
+            });
+            // })
+          })
+        }
         title={tableTitle}
-        isLoading={isLoading || isFetching}
+        // isLoading={isLoading || isFetching}
         localization={{
           toolbar: { searchPlaceholder: "Buscar", searchTooltip: "Buscar" },
           body: { emptyDataSourceMessage: "No hay informacion disponible" },
@@ -76,12 +103,14 @@ const TableFormat = (props) => {
                   formTarget={formTarget}
                   buttonLabel="Editar"
                   categories={formTarget === "dish" && categories}
+                  onRefresh={refresh}
                 />
 
                 <DeleteButton
                   variant="contained"
                   onClick={() => {
                     onDelete(props.data.id);
+                    refresh();
                   }}
                 >
                   <FaTrash />
@@ -99,6 +128,13 @@ const TableFormat = (props) => {
           },
         }}
       />
+      <button
+        onClick={() => {
+          refresh();
+        }}
+      >
+        ok
+      </button>
     </React.Fragment>
   );
 };
