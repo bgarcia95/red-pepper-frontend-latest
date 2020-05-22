fe23a0e1cd72c280b6821a4884e5fe7ac24ce651
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-21 21:41:51 -0600

    Removing title from Page Container

--- src/components/PageContainer/PageContainer.jsx
+++ src/components/PageContainer/PageContainer.jsx
@@ -8,16 +8,6 @@ const PageContainer = (props) => {
   return (
     <React.Fragment>
       <Container>
-        <Typography variant="h5" align="center">
-          {pageTitle}
-        </Typography>
-        <Divider style={{ margin: "2rem 0" }} />
-
-        {isLoadingData && (
-          <div className="error--message">
-            <p>Hubo un problema cargando la informacion...</p>
-          </div>
-        )}
         <TableFormat {...props} />
       </Container>
     </React.Fragment>
