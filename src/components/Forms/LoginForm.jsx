e109571b0dd648c8dc5bfa47926ebaac160084a0
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-26 19:21:10 -0600

    Adding functionality to show password

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import { TextField, IconButton, InputAdornment } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { AddButton } from "components/UI/Buttons/Buttons";
 import { useDispatch } from "react-redux";
 import { loginAction } from "redux/actions/auth/auth";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 const LoginForm = (props) => {
   const { history, error } = props;
   const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = () => setShowPassword(!showPassword);
 
   return (
     <Formik
@@ -74,11 +79,25 @@ const LoginForm = (props) => {
                   fullWidth
                   name="password"
                   label="Contraseña"
-                  type="password"
                   id="password"
                   value={values.name}
                   onChange={handleChange}
                   onBlur={handleBlur}
+                  type={showPassword ? "text" : "password"}
+                  InputProps={{
+                    // <-- This is where the toggle button is added.
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
                 />
 
                 <AddButton
