501851d91b3ec8057a91143520c7fabc447afe42
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-27 19:47:34 -0600

    Adding grid for buttons

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -4,13 +4,31 @@ import { Container, Grid, Button } from "@material-ui/core";
 const Orders = () => {
   return (
     <Container>
-      <Grid container alignContent="center" spacing={2}>
-        <Grid item md={6}>
-          <Button>Boton 1</Button>
+      <Grid container direction="row" spacing={2}>
+        <Grid md={4}></Grid>
+        <Grid container direction="column" spacing={2} md={4}>
+          <Grid item>
+            <Button variant="contained" fullWidth>
+              Boton 1
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" fullWidth>
+              Boton 2
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" fullWidth>
+              Boton 3
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" fullWidth>
+              Boton 4
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item md={6}>
-          <Button>Boton 2</Button>
-        </Grid>
+        <Grid md={4}></Grid>
       </Grid>
     </Container>
   );
