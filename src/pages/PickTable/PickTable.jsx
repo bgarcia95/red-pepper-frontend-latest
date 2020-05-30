ea8dafd326a0b86a11d80b46bdd6f21292a983da
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-30 17:38:36 -0600

    Using pick table dialog to display tables

--- src/pages/PickTable/PickTable.jsx
+++ src/pages/PickTable/PickTable.jsx
@@ -1,79 +1,16 @@
 import React, { useState, useEffect } from "react";
-import {
-  IconButton,
-  CardHeader,
-  Grid,
-  Divider,
-  Button,
-  Container,
-} from "@material-ui/core";
+import { IconButton, Grid, Divider, Container } from "@material-ui/core";
 import { FaArrowLeft } from "react-icons/fa";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { useDispatch, useSelector } from "react-redux";
 import { getTablesAction } from "redux/actions/tables/tables";
 import { v4 as uuid } from "uuid";
-import { green, red } from "@material-ui/core/colors";
+import PickTableDialog from "components/Modals/PickTableDialog";
 
 const useStyles = makeStyles({
-  available: {
-    backgroundColor: green[500],
-    "&:hover": {
-      backgroundColor: green[500],
-      transform: "scale(1.1)",
-      transition: ".2s ease-in-out",
-    },
-    width: "300px",
-    color: "white",
-  },
-
-  unavailable: {
-    backgroundColor: red[500],
-    "&:hover": {
-      backgroundColor: red[500],
-      transform: "scale(1.1)",
-      transition: ".2s ease-in-out",
-    },
-    width: "300px",
-    color: "white",
-  },
   centerTitle: {
     textAlign: "center",
-  },
-  title: {
-    fontSize: 14,
-    backgroundColor: "white",
-    borderTopLeftRadius: "4px",
-    borderTopRightRadius: "4px",
-  },
-  cardContent: {
-    borderTop: "1px solid #ccc",
-    borderBottom: "1px solid #ccc",
-    backgroundColor: "white",
-  },
-  content: {
-    display: "flex",
-    justifyContent: "space-around",
-  },
-
-  root: {
-    width: "300px",
-    backgroundColor: "transparent",
-    borderColor: "rgba(0,0,0,0.02)",
-    overflow: "visible",
-  },
-  footerAvailable: {
-    backgroundColor: green[500],
-    color: "white",
-    justifyContent: "center",
-  },
-  footerUnavailable: {
-    backgroundColor: red[500],
-    color: "white",
-    justifyContent: "center",
   },
 });
 
@@ -93,13 +30,13 @@ const PickTable = (props) => {
     setTables(tablesStore);
   }, [tablesStore]);
 
-  const handleAvailability = (id) => {
-    setTables(
-      tables.map((table) =>
-        table.id === id ? (table = { ...table, isAvailable: false }) : table
-      )
-    );
-  };
+  // const handleAvailability = (id) => {
+  //   setTables(
+  //     tables.map((table) =>
+  //       table.id === id ? (table = { ...table, isAvailable: false }) : table
+  //     )
+  //   );
+  // };
 
   return (
     <div>
@@ -128,65 +65,7 @@ const PickTable = (props) => {
           {tables.map((table) => (
             <React.Fragment key={uuid()}>
               <Grid item md={4}>
-                <Button
-                  onClick={() => {
-                    console.log(`Hey ${table.id}! `);
-                    handleAvailability(table.id);
-                  }}
-                  className={
-                    table.isAvailable === false
-                      ? classes.unavailable
-                      : classes.available
-                  }
-                >
-                  <Card className={classes.root} elevation={0}>
-                    <CardHeader title={table.name} className={classes.title} />
-                    <CardContent className={classes.cardContent}>
-                      <Grid container spacing={2}>
-                        <Grid item md={4} style={{ textAlign: "right" }}>
-                          <Typography
-                            variant="body2"
-                            component="p"
-                            style={{ fontWeight: "bold" }}
-                          >
-                            Ubicación:
-                          </Typography>
-                        </Grid>
-                        <Grid item md={8} style={{ textAlign: "left" }}>
-                          <Typography variant="body2" component="p">
-                            {table.description}
-                          </Typography>
-                        </Grid>
-
-                        <Grid item md={4} style={{ textAlign: "right" }}>
-                          <Typography
-                            variant="body2"
-                            component="p"
-                            style={{ fontWeight: "bold" }}
-                          >
-                            Sillas:
-                          </Typography>
-                        </Grid>
-                        <Grid item md={8} style={{ textAlign: "left" }}>
-                          <Typography variant="body2" component="p">
-                            {table.chairs}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </CardContent>
-                    <CardActions
-                      className={
-                        table.isAvailable === false
-                          ? classes.footerUnavailable
-                          : classes.footerAvailable
-                      }
-                    >
-                      {table.isAvailable === false
-                        ? "NO DISPONIBLE"
-                        : "DISPONIBLE"}
-                    </CardActions>
-                  </Card>
-                </Button>
+                <PickTableDialog table={table} />
               </Grid>
             </React.Fragment>
           ))}
