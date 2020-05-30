3f519b2ae213c8b3b52949e942ffc00315bb1921
Brayan Garcia <bgarcia.95@outlook.com>
2020-05-29 18:48:45 -0600

    Adding dummy data for tests

---
+++ src/tests/fixtures/supplies.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: "PAN PARA HAMBURGUESA",
+    description: "PARA ELABORAR HAMBURGUESAS",
+    minimumQty: 15,
+    presentation: "UNIDADES",
+    unitOfMeasure: "UNIDAD",
+  },
+  {
+    id: 2,
+    name: "DISCOS DE CARNE",
+    description: "PARA ELABORAR HAMBURGUESAS",
+    minimumQty: 10,
+    presentation: "UNIDADES",
+    unitOfMeasure: "UNIDAD",
+  },
+  {
+    id: 3,
+    name: "PAPAS",
+    description: "PARA PAPAS",
+    minimumQty: 5,
+    presentation: "UNIDADES",
+    unitOfMeasure: "UNIDAD",
+  },
+];
