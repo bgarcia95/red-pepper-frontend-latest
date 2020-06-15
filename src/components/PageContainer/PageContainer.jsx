a2f168117c741ea6a877c380ce44e1a9ced9a527
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-15 00:54:03 -0600

    Removing Container from PageContainer component

--- src/components/PageContainer/PageContainer.jsx
+++ src/components/PageContainer/PageContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TableFormat from "components/Table/TableFormat";
-import { Typography, Container, Divider, Grid } from "@material-ui/core";
+import { Typography, Divider, Grid } from "@material-ui/core";
 import FormDialog from "components/Modals/FormDialog";
 import PropTypes from "prop-types";
 
@@ -15,31 +15,29 @@ const PageContainer = (props) => {
 
   return (
     <React.Fragment>
-      <Container>
-        <Divider style={{ margin: "1rem 0 2rem 0" }} />
-        <Typography variant="h5" align="center">
-          {pageTitle}
-        </Typography>
-        <Divider style={{ margin: "2rem 0" }} />
-        <Grid item xs={12} className="text-center">
-          <FormDialog
-            formTarget={formTarget}
-            buttonLabel={buttonLabel}
-            title={dialogTitle}
-            categories={props.formTarget === "dish" && props.categories}
-            supplies={props.formTarget === "dish" && props.supplies}
-            dishes={props.formTarget === "combo" && props.dishes}
-          />
-        </Grid>
-        <Grid item xs={12} style={{ margin: "2rem 0 1rem 0" }}>
-          {hasErrorLoadingData && (
-            <div className="error--message">
-              <p>Hubo un problema cargando la informacion...</p>
-            </div>
-          )}
-        </Grid>
-        <TableFormat {...props} />
-      </Container>
+      <Divider style={{ margin: "1rem 0 2rem 0" }} />
+      <Typography variant="h5" align="center">
+        {pageTitle}
+      </Typography>
+      <Divider style={{ margin: "2rem 0" }} />
+      <Grid item xs={12} className="text-center">
+        <FormDialog
+          formTarget={formTarget}
+          buttonLabel={buttonLabel}
+          title={dialogTitle}
+          categories={props.formTarget === "dish" && props.categories}
+          supplies={props.formTarget === "dish" && props.supplies}
+          dishes={props.formTarget === "combo" && props.dishes}
+        />
+      </Grid>
+      <Grid item xs={12} style={{ margin: "2rem 0 1rem 0" }}>
+        {hasErrorLoadingData && (
+          <div className="error--message">
+            <p>Hubo un problema cargando la informacion...</p>
+          </div>
+        )}
+      </Grid>
+      <TableFormat {...props} />
     </React.Fragment>
   );
 };
