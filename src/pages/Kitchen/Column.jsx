6f7b7a5a2843f127c1bdf12e51fbcfb8477078f5
Brayan Garcia <bgarcia.95@outlook.com>
2020-08-06 02:26:23 -0600

    Adding Column component

---
+++ src/pages/Kitchen/Column.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+import Task from "./Task";
+
+const Column = (props) => {
+  return (
+    <div
+      style={{
+        margin: "8px",
+        border: "1px solid lightgrey",
+        borderRadius: "2px",
+        width: "250px",
+
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          padding: 8,
+          borderBottom: "1px solid lightgrey",
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h5">{props.column.title}</Typography>
+      </div>
+      <div
+        style={{
+          padding: "8px",
+          transition: "background-color 0.2s ease",
+          backgroundColor: "white",
+          flexGrow: 1,
+          minHeight: "100px",
+        }}
+      >
+        {props.tasks.map((task, index) => {
+          return (
+            <Task
+              key={task.id}
+              task={task}
+              index={index}
+              combos={props.combos}
+              dishes={props.dishes}
+              status={props.column.status}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Column;
