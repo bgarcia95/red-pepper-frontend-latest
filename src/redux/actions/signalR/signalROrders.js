3394f0090ba554a9ba9443e83e3981d254cc5fbb
Brayan Garcia <bgarcia.95@outlook.com>
2021-04-21 21:51:25 -0600

    add fetchOrdersKitchen action creator

--- src/redux/actions/signalR/signalROrders.js
+++ src/redux/actions/signalR/signalROrders.js
@@ -1,30 +1,43 @@
 import http from "services/httpService";
 import {
-  ADD_ORDER_SIGNALR,
-  UPDATE_ORDER_SIGNALR,
-  FETCH_ORDERS,
-  UPDATE_ORDER_DETAILS_STATE,
+	ADD_ORDER_SIGNALR,
+	UPDATE_ORDER_SIGNALR,
+	FETCH_ORDERS,
+	UPDATE_ORDER_DETAILS_STATE,
 } from "redux/utils/actions";
 
 export const fetchOrders = () => {
-  return (dispatch) => {
-    http.get("/order").then((response) => {
-      dispatch({ type: FETCH_ORDERS, orders: response.data });
-    });
-  };
+	return (dispatch) => {
+		http.get("/order").then((response) => {
+			dispatch({ type: FETCH_ORDERS, orders: response.data });
+		});
+	};
 };
 
+export const fetchOrdersKitchen = () => {
+	return (dispatch) => {
+		http.get("http://localhost:5000/api/Kitchen").then((response) => {
+			dispatch({ type: FETCH_ORDERS, orders: response.data });
+		});
+	};
+}
+
 export const addOrderSignalR = (order) => ({
-  type: ADD_ORDER_SIGNALR,
-  order,
+	type: ADD_ORDER_SIGNALR,
+	order,
 });
 
 export const updateOrderSignalR = (order) => ({
-  type: UPDATE_ORDER_SIGNALR,
-  order,
+	type: UPDATE_ORDER_SIGNALR,
+	order,
 });
 
-export const updateOrderDetailsSignalR = (updatedDetails) => ({
-  type: UPDATE_ORDER_DETAILS_STATE,
-  updatedDetails,
-});
+export const updateOrderDetailsSignalR = (updatedDetails) => {
+
+	return  async dispatch => {
+		dispatch({
+			type: UPDATE_ORDER_DETAILS_STATE,
+			updatedDetails,
+		})
+	}
+};
