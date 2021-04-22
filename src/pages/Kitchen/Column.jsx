56269b76a20ce4683111a029131e4b9266db733e
Brayan Garcia <bgarcia.95@outlook.com>
2021-04-21 21:45:36 -0600

    fix styles

--- src/pages/Kitchen/Column.jsx
+++ src/pages/Kitchen/Column.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React from 'react';
+import { Typography } from '@material-ui/core';
 
-import Task from "./Task";
-import { green, orange, red } from "@material-ui/core/colors";
+import Task from './Task';
+import { green, orange, red } from '@material-ui/core/colors';
 
 const Column = (props) => {
   const getBgColor = (title) => {
     switch (title) {
-      case "En Cola":
+      case 'En Cola':
         return red[500];
 
-      case "En Proceso":
+      case 'En Proceso':
         return orange[500];
 
-      case "Finalizado":
+      case 'Finalizado':
         return green[500];
 
       default:
-        return "inherit";
+        return 'inherit';
     }
   };
 
@@ -39,49 +39,49 @@ const Column = (props) => {
     })
     .filter((det) => det > -1);
 
+
   return (
     <div
       style={{
-        margin: "8px",
-        border: "1px solid lightgrey",
-        borderRadius: "2px",
-        minWidth: "360px",
-
-        maxHeight: "75vh",
-
-        display: "flex",
-        flexDirection: "column",
+        margin: '8px',
+        border: '1px solid lightgrey',
+        borderRadius: '2px',
+        // overflow: 'hidden',
+        minWidth: '300px',
+        maxHeight: '90vh',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
       <div
         style={{
           padding: 8,
-          borderBottom: "1px solid lightgrey",
-          textAlign: "center",
+          borderBottom: '1px solid lightgrey',
+          textAlign: 'center',
           backgroundColor: getBgColor(props.column.title),
         }}
       >
-        <Typography variant="h5" style={{ color: "#fff" }}>
+        <Typography variant="h5" style={{ color: '#fff' }}>
           {props.column.title}
         </Typography>
       </div>
       <div
         style={{
-          padding: "8px",
-          transition: "background-color 0.2s ease",
-          backgroundColor: "white",
+          padding: '8px',
+          transition: 'background-color 0.2s ease',
+          backgroundColor: 'white',
           flexGrow: 1,
-          minHeight: "100px",
-          overflowY: "scroll",
+          minHeight: '100px',
+          overflowY: 'scroll',
         }}
       >
         {props.orders.length === 0 ? (
-          <div style={{ textAlign: "center" }}>
+          <div style={{ textAlign: 'center' }}>
             No existen ordenes actualmente
           </div>
         ) : (
           inQueueDetails.length === 0 && (
-            <p style={{ textAlign: "center" }}>
+            <p style={{ textAlign: 'center' }}>
               No existen detalles actualmente
             </p>
           )
